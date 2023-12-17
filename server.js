cd879f746fe87166fa373541d09c7c0a44694d5f
Nicolas Jurquet <nicolor01@hotmail.com>
2023-12-17 15:07:46 +0100

    Set basic structure for exam

--- server.js
+++ server.js
@@ -3,30 +3,24 @@ import TV from "./models/TV.js";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 
 app.get("/", async (req, res) => {
     const wishList = await TV.loadMany({ bought: 0 });
-    const boughtList = await TV.loadMany({ bought: 1 });
-    const balance = boughtList.reduce((acc, tv) => acc + tv.price, 0);
-    res.render("wishlist.ejs", { wishList: wishList, boughtList: boughtList, balance: balance });
+    res.render("wishlist.ejs", { wishList: wishList });
 });
 
 app.post("/add", async (req, res) => {
     const tv = new TV();
-    tv.brand = req.body.brand;
-    tv.size = req.body.size;
-    tv.price = req.body.price;
-    tv.bought = 0;
+    // tv.brand = req.body.brand;
+    // tv.size = req.body.size;
+    // tv.price = req.body.price;
+    // tv.bought = 0;
+    tv.update({ brand: req.body.brand, size: req.body.size, price: req.body.price, bought: 0 });
     await tv.save();
-    res.redirect("/");
-});
-
-app.get("/buy/:id", async (req, res) => {
-    const tv = await TV.load({ id: req.params.id });
-    tv.bought = 1;
-    await tv.save();
+    const newTV = await TV.load({ id: req.params.id });
     res.redirect("/");
 });
 
